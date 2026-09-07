753681c4c8504ea72a10b4cf13ca76c70d0a64a2
Tak943 <takf05121@gamil.com>
2026-09-08 02:54:48 +0900

    feat: Dockerに対応させた。バックエンドとフロントエンド両方。コンテナにまとめたのはバックエンドだけ。フロントはVercelのサーバーレス環境を想定。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,8 @@ import axios from 'axios' // 【追加】axiosをインポート
 // 【追加】すべての通信の基本URLをここで一括設定！
 // ※これで、各Vueファイルに書いていた axios.defaults.baseURL = ... は不要になります！
 // axios.defaults.baseURL = 'http://localhost:8080';
-axios.defaults.baseURL = 'http://192.168.3.28:8080';
+//Vercel環境下ではVercelで設定したRendar環境下のSpring BootのURLが入り、ローカル環境下では.envの8080ポートのURLが入る
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 // 【追加】アプリ起動時に、ブラウザの記憶からトークンを取り出してセットする
 const token = localStorage.getItem('app_token');
